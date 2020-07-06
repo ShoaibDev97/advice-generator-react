8b246d4407cf6dfd04863e11f0d8e7605be3c35f
Shoaib <shoaibmuhammadarif@gmail.com>
2020-07-06 11:04:47 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Loader from "react-loader-spinner";
+class App extends Component {
+  state = {
+    advice: "",
+    load: true,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount = () => {
+    this.getAdvice();
+  };
+
+  getAdvice = async () => {
+    this.setState({ advice: "", load: true });
+
+    await fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => this.setState({ advice: data.slip.advice, load: false }));
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <div className="card">
+          <p>{this.state.advice}</p>
+        </div>
+
+        <button type="button" className="btn" onClick={this.getAdvice}>
+          {!this.state.load ? (
+            "Generate"
+          ) : (
+            <Loader type="ThreeDots" color="white" height={20} width={50} />
+          )}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;

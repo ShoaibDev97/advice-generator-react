1134a7c0a4d94d3f745180fd117667ee25902d5b
ShoaibDev97 <shoaibmuhammadarif@gmail.com>
2021-07-12 13:26:13 +0500

    Updates

---
+++ precache-manifest.18ca5746efbd38a1e1ce0f0670698537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1e17a2447661cf8ba8721833ed25dd",
+    "url": "/advice-generator-react/index.html"
+  },
+  {
+    "revision": "9472b69be2c90952f776",
+    "url": "/advice-generator-react/static/css/main.61f31cd1.chunk.css"
+  },
+  {
+    "revision": "ce1748f1fcd9c13f3f7b",
+    "url": "/advice-generator-react/static/js/2.b69492b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-generator-react/static/js/2.b69492b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9472b69be2c90952f776",
+    "url": "/advice-generator-react/static/js/main.0fe548eb.chunk.js"
+  },
+  {
+    "revision": "a9565c1906e104dc2008",
+    "url": "/advice-generator-react/static/js/runtime-main.c7c35d33.js"
+  },
+  {
+    "revision": "7423684f1cd345786468c08753085c32",
+    "url": "/advice-generator-react/static/media/leaf.7423684f.jpg"
+  }
+]);
